9791fff262f8ea49a41eae9291e8a2bcb76035a9
zackbatl <54919770+zackbatl@users.noreply.github.com>
2019-09-18 19:25:53 -0600

    Add files via upload

---
+++ lab3_script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Authors : Your Name & Your partner's name
+# Date: 1/1/2019
+
+#Problem 1 Code:
+#Make sure to document how you are solving each problem!
+
+echo "Enter regular expression and file name"
+read regex_input filename
+
+grep $regex_input $filename
+
+grep -c -P  '[0-9]{3}-[0-9]{3}-[0-9]{4}' regex_practice.txt
